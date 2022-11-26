4ca32fcbc43dda30aca9988dfb902fb08c6adc1d
damianclase <72443172+damianclase@users.noreply.github.com>
2022-11-26 19:13:15 +0100

    Delete fibonacci/src/main/java/es/iescarrillo/daw/endes/fibonacci directory

--- fibonacci/src/main/java/es/iescarrillo/daw/endes/fibonacci/FibonacciMain.java
+++
@@ -1,7 +0,0 @@
-package es.iescarrillo.daw.endes.fibonacci;
-public class FibonacciMain {
-	public static void main(String[] args) {
-		Fibonacci numeros = new Fibonacci();
-		System.out.println(numeros.metodo());
-	}
-}
